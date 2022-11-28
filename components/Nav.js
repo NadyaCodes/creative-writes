adc5a26611a538af87a8f66c65e263397b68bdc4
NadyaCodes <nadyacorscadden@gmail.com>
2022-11-28 11:45:38 -0600

    create and style join button and route

--- components/Nav.js
+++ components/Nav.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
+
 export default function Nav() {
   return (
-    <div>
-      <h1>Nav</h1>
-    </div>
+    <nav className="flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">Creative Minds</button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        <Link
+          href={"/auth/login"}
+          className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8"
+        >
+          Join Now
+        </Link>
+      </ul>
+    </nav>
   );
 }
 
-//currently at 12:48 with https://www.youtube.com/watch?v=o7jTN4s78GQ&t=16s
+//currently at 27:13 with https://www.youtube.com/watch?v=o7jTN4s78GQ&t=16s
